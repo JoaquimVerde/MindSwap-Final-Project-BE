2df69dc17f7133f8cfdaa510a8b52db5a020aeb6
José Ruy Sá Ribeiro <joseruysaribeiro@icloud.com>
2024-03-24 20:22:57 +0000

    created folder structure for app

--- applicationssuite/src/test/java/org/acme/getting/startedIT.java
+++
@@ -1,8 +0,0 @@
-package org.acme.getting;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class startedIT extends startedTest {
-    // Execute the same tests but in packaged mode.
-}
