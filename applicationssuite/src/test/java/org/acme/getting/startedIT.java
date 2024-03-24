c53d7d7436a86ae5b8746a2cc21c7181085f9f86
José Ruy Sá Ribeiro <joseruysaribeiro@icloud.com>
2024-03-24 20:15:16 +0000

    created quarkus app

---
+++ applicationssuite/src/test/java/org/acme/getting/startedIT.java
@@ -0,0 +1,8 @@
+package org.acme.getting;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class startedIT extends startedTest {
+    // Execute the same tests but in packaged mode.
+}
