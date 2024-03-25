032d1ceb71425ba7abb4b90eb7b78c84217b98b6
fguedes10 <fadg100@gmail.com>
2024-03-25 09:27:47 +0000

    fix some text erros

--- src/main/java/com/mindera/finalproject/be/entity/User.java
+++ src/main/java/com/mindera/finalproject/be/entity/User.java
@@ -1,4 +1,30 @@
 package com.mindera.finalproject.be.entity;
 
-public class User {
+import java.time.LocalDate;
+import java.time.Period;
+
+public abstract class User {
+
+    private Long id;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+
+    private LocalDate dateOfBirth;
+
+    private int age;
+
+    private String address;
+
+
+    private int calcAge(){
+        LocalDate currentDate = LocalDate.now();
+        return Period.between(dateOfBirth, currentDate).getYears();
+    }
+
 }
