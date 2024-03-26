92fdc1d8ce02e4e7a907c30d800bf7058f7bca94
fguedes10 <fadg100@gmail.com>
2024-03-26 15:39:32 +0000

    create barebone ProjectServiceImpl

---
+++ src/main/java/com/mindera/finalproject/be/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,24 @@
+package com.mindera.finalproject.be.service.impl;
+
+import com.mindera.finalproject.be.dto.project.ProjectCreateDto;
+import com.mindera.finalproject.be.dto.project.ProjectPublicDto;
+import com.mindera.finalproject.be.service.ProjectService;
+
+public class ProjectServiceImpl implements ProjectService {
+
+
+    @Override
+    public ProjectPublicDto findAll() {
+        return null;
+    }
+
+    @Override
+    public ProjectPublicDto findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public ProjectCreateDto createProject(ProjectCreateDto ProjectCreateDto) {
+        return null;
+    }
+}
