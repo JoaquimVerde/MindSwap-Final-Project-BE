26760807b7a50e7bb616fd8ed248f9a6a14c45d9
fguedes10 <fadg100@gmail.com>
2024-03-26 12:21:47 +0000

    change Project entity put List longs students id, create publicProjectDto

--- src/main/java/com/mindera/finalproject/be/dto/project/createProjectDto.java
+++ src/main/java/com/mindera/finalproject/be/dto/project/createProjectDto.java
@@ -1,10 +1,16 @@
 package com.mindera.finalproject.be.dto.project;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.URL;
+
 public record createProjectDto(
 
-
+        @NotEmpty(message = "This field cannot be empty")
+        @Pattern(regexp = "^[a-zA-Z_0-9~`'^ ]+$", message = "Invalid name")
         String name,
 
+        @URL(message = "invalid url")
         String gitHubRepo
 
 ) {
--- src/main/java/com/mindera/finalproject/be/dto/project/publicProjectDto.java
+++ src/main/java/com/mindera/finalproject/be/dto/project/publicProjectDto.java
@@ -1,13 +1,17 @@
 package com.mindera.finalproject.be.dto.project;
 
+import com.mindera.finalproject.be.entity.Course;
+import com.mindera.finalproject.be.entity.Student;
+
 import java.util.List;
 
 public record publicProjectDto(
 
-
-
-
-
+        Long id,
+        List<Student> firstName, //change to studentPublicDto
+        Course courseName, // change to coursePublicDto
+        String name,
+        String gitHubRepo
 
 ) {
 }
