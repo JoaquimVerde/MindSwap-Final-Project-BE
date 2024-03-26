77f01101333c6409beb57819f14d7b79a380c90d
José Ruy Sá Ribeiro <22319070+jruysaribeiro@users.noreply.github.com>
2024-03-26 11:03:17 +0000

    minor issue with Person constructor and Staff super Constructor, fixed.

--- src/main/java/com/mindera/finalproject/be/entity/Staff.java
+++ src/main/java/com/mindera/finalproject/be/entity/Staff.java
@@ -2,10 +2,12 @@ package com.mindera.finalproject.be.entity;
 
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 
+import java.time.LocalDate;
+
 @DynamoDbBean
 public class Staff extends Person{
 
-    public Staff() {
-        super();
+    public Staff(Long id, String email, String firstName, String lastName, String role, String username, LocalDate dateOfBirth, int age, String address) {
+        super(id, email, firstName, lastName, role, username, dateOfBirth, age, address);
     }
 }
