f0ecc17a3fceb9a6fcf2cbe9efa4e5b7303b2afe
José Ruy Sá Ribeiro <22319070+jruysaribeiro@users.noreply.github.com>
2024-03-26 12:21:00 +0000

    created Registration Controller

--- src/main/java/com/mindera/finalproject/be/controller/RegistrationController.java
+++ src/main/java/com/mindera/finalproject/be/controller/RegistrationController.java
@@ -15,14 +15,44 @@ public class RegistrationController {
     @Inject
     RegistrationServiceImpl registrationService;
 
+
+
+    @GET
+    public Response getRegistrations() {
+        return Response.ok(registrationService.getRegistrations()).build();
+    }
+
+
+    @Path("/registration/create")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @POST
     public Response createRegistration(RegistrationCreateDto registrationCreateDto) {
         return Response.ok(registrationService.createRegistration(registrationCreateDto)).build();
     }
 
+    @Path("/registration/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @GET
-    public Response getRegistrations() {
-        return Response.ok(registrationService.getRegistrations()).build();
+    public Response getRegistration(@PathParam("id") Long id) { //Este id é personId#courseId, deve ser concatenado antes de chegar aqui
+        return Response.ok(registrationService.getRegistration(id)).build();
+    }
+
+    @Path("/registration/{id}/edit")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @PUT
+    public Response editRegistration(@PathParam("id") Long id, RegistrationCreateDto registrationCreateDto) { //Este id é personId#courseId, deve ser concatenado antes de chegar aqui
+        return Response.ok(registrationService.editRegistration(id, registrationCreateDto)).build();
+    }
+
+    @Path("/registration/{id}/delete")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @DELETE
+    public Response deleteRegistration(@PathParam("id") Long id) { //Este id é personId#courseId, deve ser concatenado antes de chegar aqui
+        return Response.ok(registrationService.deleteRegistration(id)).build();
     }
 
 }
