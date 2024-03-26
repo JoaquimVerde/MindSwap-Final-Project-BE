139659bb30d1e4717e5c68a465f7d2640c568d7c
José Ruy Sá Ribeiro <22319070+jruysaribeiro@users.noreply.github.com>
2024-03-26 11:14:02 +0000

    updated dependencies

--- src/main/java/com/mindera/finalproject/be/dto/registration/RegistrationCreateDto.java
+++ src/main/java/com/mindera/finalproject/be/dto/registration/RegistrationCreateDto.java
@@ -1,6 +1,10 @@
 package com.mindera.finalproject.be.dto.registration;
 
+
+import jakarta.validation.constraints.NotNull;
+
 public record RegistrationCreateDto(
+        @NotNull
         Long personId,
         Long courseId,
         String status,
