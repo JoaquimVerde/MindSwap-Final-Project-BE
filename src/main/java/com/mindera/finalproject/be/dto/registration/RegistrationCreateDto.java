23ff5de5f392ba9f401c66be604c2c6a1631f82f
José Ruy Sá Ribeiro <22319070+jruysaribeiro@users.noreply.github.com>
2024-03-26 11:17:29 +0000

    registration create dto validations with Jakarta

--- src/main/java/com/mindera/finalproject/be/dto/registration/RegistrationCreateDto.java
+++ src/main/java/com/mindera/finalproject/be/dto/registration/RegistrationCreateDto.java
@@ -1,14 +1,22 @@
 package com.mindera.finalproject.be.dto.registration;
 
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record RegistrationCreateDto(
         @NotNull
         Long personId,
+
+        @NotNull
         Long courseId,
+
+        @NotBlank
         String status,
+
         String finalGrade,
+
+        @NotBlank
         Boolean active
 ) {
 }
