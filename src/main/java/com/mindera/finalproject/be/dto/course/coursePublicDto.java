ecb55ed6b3c6a939ba4a8fbd8f7ee84cf829c001
mtsluis <151209805+mtsluis@users.noreply.github.com>
2024-03-26 11:59:38 +0000

    added the public DTO for Course

--- src/main/java/com/mindera/finalproject/be/dto/course/coursePublicDto.java
+++ src/main/java/com/mindera/finalproject/be/dto/course/coursePublicDto.java
@@ -1,4 +1,32 @@
 package com.mindera.finalproject.be.dto.course;
 
-public class coursePublicDto {
+import com.mindera.finalproject.be.entity.Person;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+public record coursePublicDto (
+
+        @Schema(description = "The course id", example = "1")
+        Long id,
+        @Schema(description = "The course name", example = "Frontend")
+        String name,
+        @Schema(description = "The course edition", example = "1")
+        Integer edition,
+        @Schema(description = "The course teacher", example = "John Doe")
+        Person teacher,
+        @Schema(description = "The course syllabus", example = "HTML, CSS, JavaScript")
+        String syllabus,
+        @Schema(description = "The course program", example = "Frontend")
+        String program,
+        @Schema(description = "The course schedule", example = "{monday=10-18, tuesday=14-18}")
+        Map<String, String> schedule,
+        @Schema(description = "The course price", example = "900.00")
+        BigDecimal price,
+        @Schema(description = "The course duration", example = "30")
+        Integer duration,
+        @Schema(description = "The course location", example = "Porto")
+        String location
+) {
 }
