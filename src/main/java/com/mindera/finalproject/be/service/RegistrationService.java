2cdc602dda8aaeda736fd9336507376c74077f48
José Ruy Sá Ribeiro <22319070+jruysaribeiro@users.noreply.github.com>
2024-03-26 12:38:53 +0000

    created RegistrationService and ServiceImpl with all methods returning null. TODO: Implement Methods

--- src/main/java/com/mindera/finalproject/be/service/RegistrationService.java
+++ src/main/java/com/mindera/finalproject/be/service/RegistrationService.java
@@ -1,4 +1,18 @@
 package com.mindera.finalproject.be.service;
 
+import com.mindera.finalproject.be.dto.registration.RegistrationCreateDto;
+import com.mindera.finalproject.be.dto.registration.RegistrationPublicDto;
+import com.mindera.finalproject.be.entity.Registration;
+
+import java.util.List;
+
 public interface RegistrationService {
+    Registration createRegistration(RegistrationCreateDto registrationCreateDto);
+    List<RegistrationPublicDto> getRegistrations();
+    RegistrationPublicDto getRegistrationByCompositeKey(String compositeKey);
+
+    RegistrationCreateDto updateRegistration(String CompositeKey, RegistrationCreateDto registrationCreateDto);
+
+    RegistrationPublicDto deleteRegistration(String compositeKey);
+
 }
