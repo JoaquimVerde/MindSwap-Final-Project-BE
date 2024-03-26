33ac3ab6bf04cdfc72a93a4d385931dc8cae62a5
Concepticus <carlos_qq_silva@hotmail.com>
2024-03-26 14:16:42 +0000

    created teacher entity class

---
+++ src/main/java/com/mindera/finalproject/be/entity/Teacher.java
@@ -0,0 +1,12 @@
+package com.mindera.finalproject.be.entity;
+
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+
+import java.time.LocalDate;
+
+@DynamoDbBean
+public class Teacher extends Staff {
+    public Teacher(Long id, String email, String firstName, String lastName, String role, String username, LocalDate dateOfBirth, int age, String address) {
+        super(id, email, firstName, lastName, role, username, dateOfBirth, age, address);
+    }
+}
