95f3ead8eb146f1f20e341adc8c30eeec0f8e819
mtsluis <151209805+mtsluis@users.noreply.github.com>
2024-03-27 10:11:51 +0000

    created CourseService interface, with basic methods

---
+++ src/main/java/com/mindera/finalproject/be/service/CourseService.java
@@ -0,0 +1,17 @@
+package com.mindera.finalproject.be.service;
+
+
+import com.mindera.finalproject.be.dto.course.CourseCreateDto;
+import com.mindera.finalproject.be.dto.course.CoursePublicDto;
+import com.mindera.finalproject.be.entity.Course;
+
+import java.util.List;
+
+public interface CourseService {
+
+    List<Course> findAll();
+    CoursePublicDto findById(Long id);
+    CoursePublicDto create(CourseCreateDto coursePublicDto);
+    CoursePublicDto edit(Long id, CourseCreateDto coursePublicDto);
+    void delete(Long id);
+}
