2cf8d86a2ac203596043434f68cef46c08668197
TiagoASGomes <150845545+TiagoASGomes@users.noreply.github.com.>
2024-03-27 10:18:21 +0000

    Fixed controllers

--- src/main/java/com/mindera/finalproject/be/controller/StudentController.java
+++ src/main/java/com/mindera/finalproject/be/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.mindera.finalproject.be.controller;
 import com.mindera.finalproject.be.dto.student.StudentCreateDto;
 import com.mindera.finalproject.be.service.StudentService;
 import jakarta.inject.Inject;
+import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -12,37 +13,37 @@ import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class StudentController {
+
     @Inject
-    StudentService studentService;
+    private StudentService studentService;
 
     @GET
-    public Response getStudents(){
+    public Response getStudents() {
         return Response.ok(studentService.findAll()).build();
     }
 
     @GET
     @Path("{studentId}")
-    public Response getStudentById(Long id){
+    public Response getStudentById(@PathParam("studentId") Long id) {
         return Response.ok(studentService.get(id)).build();
     }
+
     @POST
-    public Response createStudent(StudentCreateDto studentCreateDto){
+    public Response createStudent(@Valid @RequestBody StudentCreateDto studentCreateDto) {
         return Response.ok(studentService.add(studentCreateDto)).status(Response.Status.CREATED).build();
     }
+
     @PUT
     @Path("{studentId}")
-    public Response editStudent(Long id, StudentCreateDto studentCreateDto){
-        return Response.accepted(studentService.edit(id, studentCreateDto)).status(Response.Status.ACCEPTED).build();
+    public Response editStudent(@PathParam("studentId") Long id, @Valid @RequestBody StudentCreateDto studentCreateDto) {
+        return Response.ok(studentService.edit(id, studentCreateDto)).build();
     }
+
     @DELETE
     @Path("{studentId}")
-    public Response deleteStudent(Long id){
+    public Response deleteStudent(@PathParam("studentId") Long id) {
         return Response.ok(studentService.delete(id)).build();
     }
-
-
-
-
 
 
 }
