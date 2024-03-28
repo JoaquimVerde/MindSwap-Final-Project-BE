d710ee4b024ff8696f8f5f7ac052d01e60b951f7
José Ruy Sá Ribeiro <22319070+jruysaribeiro@users.noreply.github.com>
2024-03-28 15:01:27 +0000

    some Registration CRUD tests 5/5 passing

--- src/main/java/com/mindera/finalproject/be/service/impl/RegistrationServiceImpl.java
+++ src/main/java/com/mindera/finalproject/be/service/impl/RegistrationServiceImpl.java
@@ -3,36 +3,66 @@ package com.mindera.finalproject.be.service.impl;
 import com.mindera.finalproject.be.dto.registration.RegistrationCreateDto;
 import com.mindera.finalproject.be.dto.registration.RegistrationPublicDto;
 import com.mindera.finalproject.be.entity.Registration;
+import com.mindera.finalproject.be.repository.RegistrationRepository;
 import com.mindera.finalproject.be.service.RegistrationService;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.modelmapper.ModelMapper;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@ApplicationScoped
 public class RegistrationServiceImpl implements RegistrationService {
+
+    private final RegistrationRepository registrationRepository;
+    private final ModelMapper modelMapper;
+
+    @Inject
+    public RegistrationServiceImpl(RegistrationRepository registrationRepository, ModelMapper modelMapper) {
+        this.registrationRepository = registrationRepository;
+        this.modelMapper = modelMapper;
+    }
 
     //TODO: Implement Methods
 
     @Override
-    public Registration createRegistration(RegistrationCreateDto registrationCreateDto) {
-        return null;
+    public Registration create(RegistrationCreateDto registrationCreateDto) {
+        Registration registration = modelMapper.map(registrationCreateDto, Registration.class);
+        return registrationRepository.save(registration);
     }
 
     @Override
-    public List<RegistrationPublicDto> findAll() {
-        return null;
+    public List<RegistrationPublicDto> getAll() {
+        return registrationRepository.getAll().stream()
+                .map(registration -> modelMapper.map(registration, RegistrationPublicDto.class))
+                .collect(Collectors.toList());
     }
 
     @Override
-    public RegistrationPublicDto findByCompositeKey(String compositeKey) {
-        return null;
+    public RegistrationPublicDto getByCompositeKey(String compositeKey) {
+        Registration registration =  registrationRepository.getByCompositeKey(compositeKey);
+        return modelMapper.map(registration, RegistrationPublicDto.class);
     }
 
     @Override
-    public RegistrationCreateDto updateRegistration(String CompositeKey, RegistrationCreateDto registrationCreateDto) {
-        return null;
+    public RegistrationPublicDto update(String compositeKey, RegistrationCreateDto registrationCreateDto) {
+        Registration existingRegistration = registrationRepository.getByCompositeKey(compositeKey);
+        if (existingRegistration != null) {
+            Registration updatedRegistration = modelMapper.map(registrationCreateDto, Registration.class);
+            registrationRepository.update(updatedRegistration);
+            return modelMapper.map(updatedRegistration, RegistrationPublicDto.class);
+        } else {
+            // Send exception "Not Found"
+            return null;
+        }
     }
 
     @Override
-    public RegistrationPublicDto deleteRegistration(String compositeKey) {
-        return null;
+    public RegistrationPublicDto delete(String compositeKey) {
+        Registration registration = registrationRepository.getByCompositeKey(compositeKey);
+        registration.setActive(false);
+        return modelMapper.map(registration, RegistrationPublicDto.class);
     }
 }
