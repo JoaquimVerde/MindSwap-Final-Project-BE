2827fc545cef3eaad0a815b1721d120a9cc6b99c
Fguedes10 <fadg100@gmail.com>
2024-03-30 22:09:49 +0000

    Add DynamoEnhanced projectService, create tests for projectServices

---
+++ src/test/java/com/mindera/finalproject/be/Project/ProjectServiceTests.java
@@ -0,0 +1,54 @@
+package com.mindera.finalproject.be.Project;
+
+import com.mindera.finalproject.be.dto.project.ProjectCreateDto;
+import com.mindera.finalproject.be.entity.Project;
+import com.mindera.finalproject.be.service.ProjectService;
+import com.mindera.finalproject.be.service.impl.ProjectServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class ProjectServiceTests {
+
+    @InjectMocks
+    ProjectServiceImpl projectService;
+
+    @Test
+     void testFindAll() {
+        List<Project> projects = projectService.findAll();
+        assertFalse(projects.isEmpty());
+    }
+
+    @Test
+     void testFindById() {
+        Project project = projectService.findById(1L);
+        assertEquals(1L, project.getId());
+    }
+
+    @Test
+     void testCreateProject() {
+        ProjectCreateDto projectCreateDto = new ProjectCreateDto("name", "gitHubRepo");
+        Project project = projectService.create(projectCreateDto);
+        assertEquals("name", project.getName());
+        assertEquals("gitHubRepo", project.getGitHubRepo());
+    }
+
+    @Test
+     void testUpdateProject() {
+        ProjectCreateDto projectCreateDto = new ProjectCreateDto("name", "gitHubRepo");
+        Project project = projectService.update(1L, projectCreateDto);
+        assertEquals("name", project.getName());
+        assertEquals("gitHubRepo", project.getGitHubRepo());
+    }
+
+    @Test
+     void testDeleteProject() {
+        projectService.delete(1L);
+    }
+
+
+}
