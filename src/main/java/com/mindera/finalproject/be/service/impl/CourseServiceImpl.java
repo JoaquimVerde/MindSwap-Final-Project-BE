c522f064d6f97c471741c8ed6bfa0f70eb236e17
mtsluis <151209805+mtsluis@users.noreply.github.com>
2024-04-01 17:48:44 +0100

    updated Course methods

--- src/main/java/com/mindera/finalproject/be/service/impl/CourseServiceImpl.java
+++ src/main/java/com/mindera/finalproject/be/service/impl/CourseServiceImpl.java
@@ -1,43 +1,55 @@
 package com.mindera.finalproject.be.service.impl;
 
 import com.mindera.finalproject.be.dto.course.CourseCreateDto;
-import com.mindera.finalproject.be.dto.course.CoursePublicDto;
+import com.mindera.finalproject.be.entity.Course;
 import com.mindera.finalproject.be.service.CourseService;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.Key;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class CourseServiceImpl implements CourseService {
 
-    @Override
-    public List<CoursePublicDto> getAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    private DynamoDbTable<Course> courseTable;
+
+    @Inject
+    void projectEnhancedService(DynamoDbEnhancedClient dynamoEnhancedClient) {
+        courseTable = dynamoEnhancedClient.table("Course", TableSchema.fromBean(Course.class));
     }
 
     @Override
-    public CoursePublicDto getById(String id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+    public List<Course> getAll() {
+        return courseTable.scan().items().stream().collect(Collectors.toList());
     }
 
     @Override
-    public CoursePublicDto create(CourseCreateDto coursePublicDto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    public Course getById(String id) {
+        return courseTable.getItem(Key.builder().partitionValue(id).build());
     }
 
     @Override
-    public CoursePublicDto update(String id, CourseCreateDto coursePublicDto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    public Course create(CourseCreateDto coursePublicDto) {
+        Course course = new Course();
+        courseTable.putItem(course);
+        return course;
+    }
+
+    @Override
+    public Course update(String id, CourseCreateDto coursePublicDto) {
+        Course course = new Course();
+        courseTable.putItem(course);
+        return course;
     }
 
     @Override
     public void delete(String id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        courseTable.deleteItem(Key.builder().partitionValue(id).build());
     }
 
 }
