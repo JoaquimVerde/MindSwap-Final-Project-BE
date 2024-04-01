7b6b02a3121bf915f0857df9751fb68533ba254e
andrenunes3 <152313579+andrenunes3@users.noreply.github.com>
2024-04-01 17:53:09 +0100

    done person get all method, and added Person converter and added 2 converters methods

---
+++ src/main/java/com/mindera/finalproject/be/converter/PersonConverter.java
@@ -0,0 +1,24 @@
+package com.mindera.finalproject.be.converter;
+
+import com.mindera.finalproject.be.dto.person.PersonPublicDto;
+import com.mindera.finalproject.be.entity.Person;
+
+import java.util.List;
+
+public class PersonConverter {
+    public static PersonPublicDto fromEntityToDto(Person person){
+        return new PersonPublicDto(
+                person.getId(),
+                person.getEmail(),
+                person.getFirstName(),
+                person.getLastName(),
+                person.getRole(),
+                person.getUsername(),
+                person.getDateOfBirth(),
+                person.getAddress()
+                );
+    }
+    public static List<PersonPublicDto> fromEntityListToDtoList(List<Person> persons){
+       return persons.stream().map(PersonConverter::fromEntityToDto).toList();
+    }
+}
--- src/main/java/com/mindera/finalproject/be/service/impl/PersonServiceImpl.java
+++ src/main/java/com/mindera/finalproject/be/service/impl/PersonServiceImpl.java
@@ -1,19 +1,33 @@
 package com.mindera.finalproject.be.service.impl;
 
+import com.mindera.finalproject.be.converter.PersonConverter;
 import com.mindera.finalproject.be.dto.person.PersonCreateDto;
 import com.mindera.finalproject.be.dto.person.PersonPublicDto;
+import com.mindera.finalproject.be.entity.Person;
 import com.mindera.finalproject.be.service.PersonService;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 
 import java.util.List;
 
 @ApplicationScoped
 public class PersonServiceImpl implements PersonService {
+    public static final String PERSON_TABLE_NAME = "Person";
+
+    private DynamoDbTable<Person> personTable;
+
+    @Inject
+    void courseEnhancedService(DynamoDbEnhancedClient dynamoEnhancedClient) {
+        personTable = dynamoEnhancedClient.table(PERSON_TABLE_NAME, TableSchema.fromBean(Person.class));
+    }
 
     @Override
     public List<PersonPublicDto> getAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+        return PersonConverter.fromEntityListToDtoList(personTable.scan().items().stream().toList());
+
     }
 
     @Override
