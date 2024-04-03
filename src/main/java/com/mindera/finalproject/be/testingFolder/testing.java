6f33647d2f7792c088810a7867f54aad3a3fbb54
tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-04-03 16:27:52 +0100

    testing yml v2

--- src/main/java/com/mindera/finalproject/be/testingFolder/testing.java
+++ src/main/java/com/mindera/finalproject/be/testingFolder/testing.java
@@ -1,5 +1,8 @@
 package com.mindera.finalproject.be.testingFolder;
 
 public class testing {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
     
 }
