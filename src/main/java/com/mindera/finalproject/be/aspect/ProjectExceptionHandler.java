05f01532755fe8808fab732988b26c5b5d2e0aa1
fguedes10 <fadg100@gmail.com>
2024-04-03 15:44:47 +0100

    create ProjectExcepctionHandler

---
+++ src/main/java/com/mindera/finalproject/be/aspect/ProjectExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.mindera.finalproject.be.aspect;
+
+import com.mindera.finalproject.be.exception.project.ProjectAlreadyExistsException;
+import com.mindera.finalproject.be.exception.project.ProjectException;
+import com.mindera.finalproject.be.exception.project.ProjectNotFoundException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+import java.util.Date;
+
+@Provider
+public class ProjectExceptionHandler implements ExceptionMapper<ProjectException> {
+    @Override
+    public Response toResponse(ProjectException e) {
+        if(e instanceof ProjectNotFoundException){
+            Error error = new Error.Builder()
+                    .message(e.getMessage())
+                    .status(Response.Status.NOT_FOUND.getStatusCode())
+                    .timestamp(new Date())
+                    .build();
+            return Response.status(Response.Status.NOT_FOUND).entity(error).build();
+        }
+        if(e instanceof ProjectAlreadyExistsException){
+            Error error = new Error.Builder()
+                    .message(e.getMessage())
+                    .status(Response.Status.CONFLICT.getStatusCode())
+                    .timestamp(new Date())
+                    .build();
+            return Response.status(Response.Status.CONFLICT).entity(error).build();
+        }
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Unexpected error in project controller").build();
+    }
+}
