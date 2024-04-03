5d4aa4bbfee9ddebd0fa5d741730ef62e9f904ac
Concepticus <carlos_qq_silva@hotmail.com>
2024-04-03 11:22:52 +0100

    starting the personserviceimpl

--- src/main/java/com/mindera/finalproject/be/service/impl/PersonServiceImpl.java
+++ src/main/java/com/mindera/finalproject/be/service/impl/PersonServiceImpl.java
@@ -1,14 +1,31 @@
 package com.mindera.finalproject.be.service.impl;
 
+import com.mindera.finalproject.be.TableCreation.TableCreation;
 import com.mindera.finalproject.be.dto.person.PersonCreateDto;
 import com.mindera.finalproject.be.dto.person.PersonPublicDto;
+import com.mindera.finalproject.be.entity.Person;
 import com.mindera.finalproject.be.service.PersonService;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 
 import java.util.List;
 
 @ApplicationScoped
 public class PersonServiceImpl implements PersonService {
+
+    private final String TABLE_NAME = "Person";
+    private final String GSIPK = "GSIPK";
+    @Inject
+    TableCreation tableCreation;
+    private DynamoDbTable<Person> personTable;
+
+    @Inject
+    void personEnchancedService(DynamoDbEnhancedClient dynamoEnhancedClient) {
+        personTable = dynamoEnhancedClient.table(TABLE_NAME, TableSchema.fromBean(Person.class));
+    }
 
     @Override
     public List<PersonPublicDto> getAll() {
