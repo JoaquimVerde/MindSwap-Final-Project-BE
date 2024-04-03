e272333028b8360639c33aba9fcd3e19c2acc8f6
pedrosoares7 <soarespedro7@hotmail.com>
2024-04-03 12:17:35 +0100

    Course Exceptions

---
+++ src/main/java/com/mindera/finalproject/be/exception/CourseExceptions/CourseAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.mindera.finalproject.be.exception.CourseExceptions;
+
+public class CourseAlreadyExistsException extends Exception{
+
+    public CourseAlreadyExistsException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/mindera/finalproject/be/exception/CourseExceptions/CourseNotFoundException.java
@@ -0,0 +1,8 @@
+package com.mindera.finalproject.be.exception.CourseExceptions;
+
+public class CourseNotFoundException extends Exception{
+
+    public CourseNotFoundException(String message) {
+        super(message);
+    }
+}
