5971eb9bb07dc91d40af65f0277ec76172f8cd0a
fguedes10 <fadg100@gmail.com>
2024-04-03 15:55:50 +0100

    add @Schema in project dtos

--- src/main/java/com/mindera/finalproject/be/dto/project/ProjectCreateDto.java
+++ src/main/java/com/mindera/finalproject/be/dto/project/ProjectCreateDto.java
@@ -2,6 +2,7 @@ package com.mindera.finalproject.be.dto.project;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.hibernate.validator.constraints.URL;
 
 import java.util.List;
@@ -10,13 +11,16 @@ public record ProjectCreateDto(
 
         @NotEmpty(message = "This field cannot be empty")
         @Pattern(regexp = "^[a-zA-Z_0-9~`'^ ]+$", message = "Invalid name")
+        @Schema(description = "The project name", example = "Frontend Project 1")
         String name,
-
+        @Schema(description = "The project student ids", example = "PERSON#asqwe-1234-asd,PERSON#asqwe-1234-asd")
         List<String> studentIds,
-
+        @NotEmpty(message = "Course is mandatory")
+        @Schema(description = "The project course id", example = "COURSE#asqwe-1234-asd")
         String courseId,
 
         @URL(message = "invalid url")
+        @Schema(description = "The project gitHub repo", example = "https://github.com/user/repo")
         String gitHubRepo
 
 
