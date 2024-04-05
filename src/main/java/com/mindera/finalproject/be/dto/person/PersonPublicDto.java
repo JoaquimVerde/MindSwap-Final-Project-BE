d11092deddfcfe5a5932ab2d0a3f07b1ca2501a3
fguedes10 <fadg100@gmail.com>
2024-04-05 16:20:27 +0100

    add schemas in person dtos

--- src/main/java/com/mindera/finalproject/be/dto/person/PersonPublicDto.java
+++ src/main/java/com/mindera/finalproject/be/dto/person/PersonPublicDto.java
@@ -1,15 +1,25 @@
 package com.mindera.finalproject.be.dto.person;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 import java.time.LocalDate;
 
 public record PersonPublicDto(
+        @Schema(description = "The person id", example = "PERSON#asdf-1234-1235")
         String id,
+        @Schema(description = "The person email", example = "example@example.com")
         String email,
+        @Schema(description = "The person first name", example = "Joaquim")
         String firstName,
+        @Schema(description = "The person last name", example = "Verde")
         String lastName,
+        @Schema(description = "The person role", example = "STUDENT")
         String role,
+        @Schema(description = "The person username", example = "badBoy")
         String username,
+        @Schema(description = "The person date of birth", example = "1990-01-01")
         LocalDate dateOfBirth,
+        @Schema(description = "The person address", example = "Rua das Andorinhas, 123")
         String address
 ) {
 }
