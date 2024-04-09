d8ce2411dfc367d412a4ed735ac230d5b13a1221
José Ruy Sá Ribeiro <22319070+jruysaribeiro@users.noreply.github.com>
2024-04-09 17:15:13 +0100

    fixed exception throws on controller

--- src/main/java/com/mindera/finalproject/be/service/RegistrationService.java
+++ src/main/java/com/mindera/finalproject/be/service/RegistrationService.java
@@ -31,7 +31,7 @@ public interface RegistrationService {
 
     RegistrationPublicDto updateStatus(String id, RegistrationUpdateStatusDto registrationUpdate) throws PersonNotFoundException, CourseNotFoundException, RegistrationNotFoundException;
 
-    RegistrationPublicDto updateGrade(String id, RegistrationUpdateGradeDto registrationUpdate) throws PersonNotFoundException, CourseNotFoundException;
+    RegistrationPublicDto updateGrade(String id, RegistrationUpdateGradeDto registrationUpdate) throws PersonNotFoundException, CourseNotFoundException,RegistrationNotFoundException;
 
 
 }
