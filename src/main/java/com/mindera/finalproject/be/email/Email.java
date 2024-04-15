524ba312785899e4847a029a53dc665e53d52219
José Ruy Sá Ribeiro <22319070+jruysaribeiro@users.noreply.github.com>
2024-04-15 12:21:53 +0100

    templates to string

--- src/main/java/com/mindera/finalproject/be/email/Email.java
+++ src/main/java/com/mindera/finalproject/be/email/Email.java
@@ -35,14 +35,132 @@ public class Email {
 
 
     public void sendWelcomeEmail(Person person) throws EmailGetTemplateException {
-        String html = getTemplate("WelcomeEmail.html");
+        String html = "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <meta content=\"width=device-width, initial-scale=1.0\" name=\"viewport\">\n" +
+                "    <title>Welcome to Our Course Platform!</title>\n" +
+                "    <style>\n" +
+                "        body {\n" +
+                "            font-family: Arial, sans-serif;\n" +
+                "            background-color: #f7f7f7;\n" +
+                "            color: #333;\n" +
+                "            padding: 20px;\n" +
+                "        }\n" +
+                "        .container {\n" +
+                "            max-width: 600px;\n" +
+                "            margin: 0 auto;\n" +
+                "            background-color: #fff;\n" +
+                "            padding: 40px;\n" +
+                "            border-radius: 8px;\n" +
+                "            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);\n" +
+                "            text-align: center; /* Center the text */\n" +
+                "        }\n" +
+                "        h1 {\n" +
+                "            color: #007bff;\n" +
+                "        }\n" +
+                "        p {\n" +
+                "            font-size: 16px;\n" +
+                "            line-height: 1.6;\n" +
+                "            margin-bottom: 20px;\n" +
+                "        }\n" +
+                "        .btn {\n" +
+                "            display: inline-block;\n" +
+                "            background-color: #007bff;\n" +
+                "            color: #fff;\n" +
+                "            padding: 10px 20px;\n" +
+                "            text-decoration: none;\n" +
+                "            border-radius: 5px;\n" +
+                "        }\n" +
+                "        .btn:hover {\n" +
+                "            background-color: #0056b3;\n" +
+                "        }\n" +
+                "        .list {\n" +
+                "            text-align: left;\n" +
+                "            margin-left: 40px;\n" +
+                "        }\n" +
+                "    </style>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<div class=\"container\">\n" +
+                "    <h1>Welcome to Acodemy Platform!</h1>\n" +
+                "    <p>Hello {{firstName}},</p>\n" +
+                "    <p>We're thrilled to have you join our community! You're now part of an exciting journey of learning and growth.</p>\n" +
+                "    <p>Here are a few things you can do:</p>\n" +
+                "    <ul class=\"list\">\n" +
+                "        <li>Explore our courses and find the perfect one for you.</li>\n" +
+                "        <li>Start learning and unlock new opportunities!</li>\n" +
+                "    </ul>\n" +
+                "    <p>Ready to get started? Click the button below to log in to your account:</p>\n" +
+                "    <p><a class=\"btn\" href=\"{{loginUrl}}\">Log In</a></p>\n" +
+                "    <p>If you have any questions or need assistance, feel free to reach out to our support team. We're here to help!</p>\n" +
+                "    <p>Happy learning!</p>\n" +
+                "    <p>The Acodemy Team</p>\n" +
+                "</div>\n" +
+                "</body>\n" +
+                "</html>";
         html = html.replace("{{firstName}}", person.getFirstName());
         html = html.replace("{{loginUrl}}", "https://fe-deployment.d1c8xxfduy22sd.amplifyapp.com/login"); // TODO CHANGE URL TO PRODUCTION URL
         mailer.send(Mail.withHtml(person.getEmail(), "Welcome to Course Applications", html));
     }
 
     public void sendCourseCandidatureStatusEmail(Person person, Course course, Registration registration) throws EmailGetTemplateException {
-        String html = getTemplate("StatusEmail.html");
+        String html = "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <meta content=\"width=device-width, initial-scale=1.0\" name=\"viewport\">\n" +
+                "    <title>Course Application Update</title>\n" +
+                "    <style>\n" +
+                "        body {\n" +
+                "            font-family: Arial, sans-serif;\n" +
+                "            background-color: #f7f7f7;\n" +
+                "            color: #333;\n" +
+                "            padding: 20px;\n" +
+                "        }\n" +
+                "        .container {\n" +
+                "            max-width: 600px;\n" +
+                "            margin: 0 auto;\n" +
+                "            background-color: #fff;\n" +
+                "            padding: 40px;\n" +
+                "            border-radius: 8px;\n" +
+                "            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);\n" +
+                "            text-align: center;\n" +
+                "        }\n" +
+                "        h1 {\n" +
+                "            color: #007bff;\n" +
+                "        }\n" +
+                "        p {\n" +
+                "            font-size: 16px;\n" +
+                "            line-height: 1.6;\n" +
+                "            margin-bottom: 20px;\n" +
+                "        }\n" +
+                "        .btn {\n" +
+                "            display: inline-block;\n" +
+                "            background-color: #007bff;\n" +
+                "            color: #fff;\n" +
+                "            padding: 10px 20px;\n" +
+                "            text-decoration: none;\n" +
+                "            border-radius: 5px;\n" +
+                "        }\n" +
+                "        .btn:hover {\n" +
+                "            background-color: #0056b3;\n" +
+                "        }\n" +
+                "    </style>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<div class=\"container\">\n" +
+                "    <h1>Course Application Update</h1>\n" +
+                "    <p>Hello {{firstName}},</p>\n" +
+                "    <p>We wanted to inform you about the status of your candidature.</p>\n" +
+                "    <p>Your application for {{courseName}} has changed to {{status}}.</p>\n" +
+                "    <p>If you have any questions or need further assistance, feel free to contact us.</p>\n" +
+                "    <p>Best regards,</p>\n" +
+                "    <p>The Acodemy Team</p>\n" +
+                "</div>\n" +
+                "</body>\n" +
+                "</html>";
         html = html.replace("{{firstName}}", person.getFirstName());
         html = html.replace("{{courseName}}", course.getName());
         html = html.replace("{{status}}", registration.getStatus());
@@ -50,7 +168,64 @@ public class Email {
     }
 
     public void sendCourseInvoice(Person person, Course course) throws EmailGetTemplateException, PdfCreateException {
-        String html = getTemplate("InvoiceEmail.html");
+        String html = "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <meta content=\"width=device-width, initial-scale=1.0\" name=\"viewport\">\n" +
+                "    <title>Invoice for Course Enrollment</title>\n" +
+                "    <style>\n" +
+                "        body {\n" +
+                "            font-family: Arial, sans-serif;\n" +
+                "            background-color: #f7f7f7;\n" +
+                "            color: #333;\n" +
+                "            padding: 20px;\n" +
+                "        }\n" +
+                "        .container {\n" +
+                "            max-width: 600px;\n" +
+                "            margin: 0 auto;\n" +
+                "            background-color: #fff;\n" +
+                "            padding: 40px;\n" +
+                "            border-radius: 8px;\n" +
+                "            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);\n" +
+                "            text-align: center;\n" +
+                "        }\n" +
+                "        h1 {\n" +
+                "            color: #007bff;\n" +
+                "        }\n" +
+                "        p {\n" +
+                "            font-size: 16px;\n" +
+                "            line-height: 1.6;\n" +
+                "            margin-bottom: 20px;\n" +
+                "        }\n" +
+                "        .btn {\n" +
+                "            display: inline-block;\n" +
+                "            background-color: #007bff;\n" +
+                "            color: #fff;\n" +
+                "            padding: 10px 20px;\n" +
+                "            text-decoration: none;\n" +
+                "            border-radius: 5px;\n" +
+                "        }\n" +
+                "        .btn:hover {\n" +
+                "            background-color: #0056b3;\n" +
+                "        }\n" +
+                "    </style>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<div class=\"container\">\n" +
+                "    <h1>Invoice for Course Enrollment</h1>\n" +
+                "    <p>Dear {{firstName}},</p>\n" +
+                "    <p>Congratulations! We are pleased to inform you that you have been accepted into the course \"{{courseName}}\".</p>\n" +
+                "    <p>As a part of your enrollment process, please find attached the invoice for the course fees. Kindly review the\n" +
+                "        details and proceed with the payment to secure your spot in the course.</p>\n" +
+                "    <p>If you have any queries regarding the invoice or need assistance with the payment process, feel free to contact\n" +
+                "        us. We're here to help!</p>\n" +
+                "    <p>Thank you for choosing our platform for your learning journey.</p>\n" +
+                "    <p>Best regards,</p>\n" +
+                "    <p>The Acodemy Team</p>\n" +
+                "</div>\n" +
+                "</body>\n" +
+                "</html>";
         html = html.replace("{{firstName}}", person.getFirstName());
         html = html.replace("{{courseName}}", course.getName());
         byte[] pdfBytes = s3Service.uploadInvoice(person, course);
@@ -58,7 +233,62 @@ public class Email {
     }
 
     public void sendEmailWithCertificate(Person person, Course course) throws EmailGetTemplateException, PdfCreateException {
-        String html = getTemplate("CertificateEmail.html");
+        String html = "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <meta content=\"width=device-width, initial-scale=1.0\" name=\"viewport\">\n" +
+                "    <title>Congratulations on Completing the Course!</title>\n" +
+                "    <style>\n" +
+                "        body {\n" +
+                "            font-family: Arial, sans-serif;\n" +
+                "            background-color: #f7f7f7;\n" +
+                "            color: #333;\n" +
+                "            padding: 20px;\n" +
+                "        }\n" +
+                "        .container {\n" +
+                "            max-width: 600px;\n" +
+                "            margin: 0 auto;\n" +
+                "            background-color: #fff;\n" +
+                "            padding: 40px;\n" +
+                "            border-radius: 8px;\n" +
+                "            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);\n" +
+                "            text-align: center;\n" +
+                "        }\n" +
+                "        h1 {\n" +
+                "            color: #007bff;\n" +
+                "        }\n" +
+                "        p {\n" +
+                "            font-size: 16px;\n" +
+                "            line-height: 1.6;\n" +
+                "            margin-bottom: 20px;\n" +
+                "        }\n" +
+                "        .btn {\n" +
+                "            display: inline-block;\n" +
+                "            background-color: #007bff;\n" +
+                "            color: #fff;\n" +
+                "            padding: 10px 20px;\n" +
+                "            text-decoration: none;\n" +
+                "            border-radius: 5px;\n" +
+                "        }\n" +
+                "        .btn:hover {\n" +
+                "            background-color: #0056b3;\n" +
+                "        }\n" +
+                "    </style>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<div class=\"container\">\n" +
+                "    <h1>Congratulations on Completing the Course!</h1>\n" +
+                "    <p>Hello {{firstName}},</p>\n" +
+                "    <p>We're thrilled to inform you that you have successfully completed the course {{courseName}}.</p>\n" +
+                "    <p>Congratulations on this achievement!</p>\n" +
+                "    <p>Your certification is attached to this email. Please download and keep it for your records.</p>\n" +
+                "    <p>If you have any questions or need further assistance, feel free to contact us.</p>\n" +
+                "    <p>Best regards,</p>\n" +
+                "    <p>The Acodemy Team</p>\n" +
+                "</div>\n" +
+                "</body>\n" +
+                "</html>";
         html = html.replace("{{studentName}}", person.getFirstName() + " " + person.getLastName());
         html = html.replace("{{courseName}}", course.getName());
         html = html.replace("{{finalDate}}", LocalDate.now().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)));
--- src/main/java/com/mindera/finalproject/be/pdf/Pdf.java
+++ src/main/java/com/mindera/finalproject/be/pdf/Pdf.java
@@ -21,7 +21,90 @@ import java.util.Locale;
 public class Pdf {
 
     public byte[] generateInvoicePdf(Person person, Course course) throws PdfCreateException {
-        String html = getTemplate("InvoiceTemplate.html");
+        String html = "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <meta content=\"width=device-width, initial-scale=1.0\" name=\"viewport\">\n" +
+                "    <title>Course Enrollment Invoice</title>\n" +
+                "    <style>\n" +
+                "        body {\n" +
+                "            font-family: Arial, sans-serif;\n" +
+                "            background-color: #f2f2f2;\n" +
+                "            color: #333;\n" +
+                "            margin: 0;\n" +
+                "            padding: 0;\n" +
+                "        }\n" +
+                "        .invoice {\n" +
+                "            width: 80%;\n" +
+                "            margin: 20px auto;\n" +
+                "            background-color: #fff;\n" +
+                "            padding: 20px;\n" +
+                "            border-radius: 10px;\n" +
+                "            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);\n" +
+                "        }\n" +
+                "        .invoice-header {\n" +
+                "            background-color: #007bff;\n" +
+                "            color: #fff;\n" +
+                "            padding: 10px;\n" +
+                "            border-top-left-radius: 10px;\n" +
+                "            border-top-right-radius: 10px;\n" +
+                "            text-align: center;\n" +
+                "        }\n" +
+                "        .invoice-body {\n" +
+                "            padding: 20px;\n" +
+                "        }\n" +
+                "        .invoice-table {\n" +
+                "            width: 100%;\n" +
+                "            border-collapse: collapse;\n" +
+                "        }\n" +
+                "        .invoice-table th, .invoice-table td {\n" +
+                "            border: 1px solid #ddd;\n" +
+                "            padding: 8px;\n" +
+                "            text-align: left;\n" +
+                "        }\n" +
+                "        .invoice-total {\n" +
+                "            margin-top: 20px;\n" +
+                "            text-align: right;\n" +
+                "            font-weight: bold;\n" +
+                "        }\n" +
+                "    </style>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<div class=\"invoice\">\n" +
+                "    <div class=\"invoice-header\">\n" +
+                "        <h2>Course Enrollment Invoice</h2>\n" +
+                "    </div>\n" +
+                "    <div class=\"invoice-body\">\n" +
+                "        <p><strong>Invoice Number:</strong> #INV-{{invoiceNumber}}</p>\n" +
+                "        <p><strong>Date:</strong> {{currentDate}}</p>\n" +
+                "        <p><strong>Student Name:</strong> {{studentName}}</p>\n" +
+                "        <p><strong>Course:</strong> {{courseName}}</p>\n" +
+                "        <table class=\"invoice-table\">\n" +
+                "            <thead>\n" +
+                "            <tr>\n" +
+                "                <th>Description</th>\n" +
+                "                <th>Quantity</th>\n" +
+                "                <th>Unit Price</th>\n" +
+                "                <th>Total</th>\n" +
+                "            </tr>\n" +
+                "            </thead>\n" +
+                "            <tbody>\n" +
+                "            <tr>\n" +
+                "                <td>Course Fee</td>\n" +
+                "                <td>1</td>\n" +
+                "                <td>{{coursePrice}}€</td>\n" +
+                "                <td>{{coursePrice}}€</td>\n" +
+                "            </tr>\n" +
+                "            </tbody>\n" +
+                "        </table>\n" +
+                "        <div class=\"invoice-total\">\n" +
+                "            <p><strong>Total:</strong> {{coursePrice}}€</p>\n" +
+                "        </div>\n" +
+                "    </div>\n" +
+                "</div>\n" +
+                "</body>\n" +
+                "</html>";
         html = html.replace("{{invoiceNumber}}", Year.now().getValue() + "/" + person.getSK().substring(7, 10) + "_" + course.getSK().substring(7, 10));
         html = html.replace("{{currentDate}}", LocalDate.now().format(DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.ENGLISH)));
         html = html.replace("{{studentName}}", person.getFirstName() + " " + person.getLastName());
@@ -33,7 +116,77 @@ public class Pdf {
     }
 
     public byte[] generateCertificatePdf(Person person, Course course) throws PdfCreateException {
-        String html = getTemplate("CertificateTemplate.html");
+        String html = "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <meta content=\"width=device-width, initial-scale=1.0\" name=\"viewport\">\n" +
+                "    <title>Conclusion Certificate</title>\n" +
+                "    <style>\n" +
+                "        @page {\n" +
+                "            size: landscape;\n" +
+                "        }\n" +
+                "        body {\n" +
+                "            font-family: Arial, sans-serif;\n" +
+                "            background-color: #fff;\n" +
+                "            color: #000;\n" +
+                "            margin: 0;\n" +
+                "            padding: 0;\n" +
+                "        }\n" +
+                "        .certificate {\n" +
+                "            width: 80%;\n" +
+                "            margin: 20px auto;\n" +
+                "            background-color: #fff;\n" +
+                "            padding: 20px;\n" +
+                "            border-radius: 10px;\n" +
+                "            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);\n" +
+                "        }\n" +
+                "        .certificate-header {\n" +
+                "            background-color: #000;\n" +
+                "            color: #fff;\n" +
+                "            padding: 10px;\n" +
+                "            border-top-left-radius: 10px;\n" +
+                "            border-top-right-radius: 10px;\n" +
+                "            text-align: center;\n" +
+                "        }\n" +
+                "        .certificate-content {\n" +
+                "            text-align: center;\n" +
+                "        }\n" +
+                "        .certificate-name {\n" +
+                "            font-size: 24px;\n" +
+                "            font-weight: bold;\n" +
+                "            margin-bottom: 20px;\n" +
+                "        }\n" +
+                "        .certificate-details {\n" +
+                "            font-size: 18px;\n" +
+                "            margin-bottom: 20px;\n" +
+                "        }\n" +
+                "        .certificate-signature {\n" +
+                "            font-size: 18px;\n" +
+                "            font-style: italic;\n" +
+                "            text-align: right;\n" +
+                "        }\n" +
+                "    </style>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<div class=\"certificate\">\n" +
+                "    <div class=\"certificate-header\">\n" +
+                "        <h2>Conclusion Certificate</h2>\n" +
+                "    </div>\n" +
+                "    <div class=\"certificate-content\">\n" +
+                "        <p class=\"certificate-name\">Certificate of Completion</p>\n" +
+                "        <p class=\"certificate-details\">This is to certify that</p>\n" +
+                "        <p class=\"certificate-details\"><strong>{{studentName}}</strong></p>\n" +
+                "        <p class=\"certificate-details\">has successfully completed the course</p>\n" +
+                "        <p class=\"certificate-details\"><strong>{{courseName}}</strong></p>\n" +
+                "        <p class=\"certificate-details\">Date of Completion: {{finalDate}}</p>\n" +
+                "    </div>\n" +
+                "    <div class=\"certificate-signature\">\n" +
+                "        <p>Signature: The Acodemy Team</p>\n" +
+                "    </div>\n" +
+                "</div>\n" +
+                "</body>\n" +
+                "</html>";
         html = html.replace("{{studentName}}", person.getFirstName() + " " + person.getLastName());
         html = html.replace("{{courseName}}", course.getName());
         html = html.replace("{{finalDate}}", LocalDate.now().format(DateTimeFormatter.ofPattern("MMMM yyyy", Locale.ENGLISH)));
